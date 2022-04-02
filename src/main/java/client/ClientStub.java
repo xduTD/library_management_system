071d49c90df54e3090ca352161ca9ab2a1e3a89e
wyz <2846797693@qq.com>
2022-04-02 23:21:39 +0800

    write some implementation for RPC.

--- src/main/java/client/ClientStub.java
+++ src/main/java/client/ClientStub.java
@@ -3,16 +3,24 @@ package client;
 import dao.Book;
 import server.Server;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientStub implements Server {
     private Socket socket;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private ObjectInputStream objIn;
+    private ObjectOutputStream objOut;
 
     /* Constructor */
     public ClientStub() {
         try {
             socket = new Socket("127.0.0.1", 8189);
+            in = new BufferedInputStream(socket.getInputStream());
+            out = new BufferedOutputStream(socket.getOutputStream());
+            objIn = new ObjectInputStream(in);
+            objOut = new ObjectOutputStream(out);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -21,24 +29,65 @@ public class ClientStub implements Server {
     /** interfaces here */
     @Override
     public boolean add(Book book) {
-        return false;
+        boolean result = true;
+        try {
+            out.write(1);
+            out.flush();
+            objOut.writeObject(book);
+            objOut.flush();
+            result = objIn.readBoolean();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
-    public boolean delete(int bookId) {
-        return false;
+    public boolean delete(int bookID) {
+        boolean result = true;
+        try {
+            out.write(2);
+            out.flush();
+            out.write(bookID);
+            out.flush();
+            result = objIn.readBoolean();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
-    public Book queryByID(int bookId) {
-        return null;
+    public Book queryByID(int bookID) {
+        Book result = null;
+        try {
+            out.write(3);
+            out.flush();
+            out.write(bookID);
+            out.flush();
+            result = (Book)objIn.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
     public Book queryByName(String bookName) {
-        return null;
-    }
+        Book result = null;
+        try {
+            out.write(4);
+            out.flush();
+            new PrintWriter(out).println(bookName);
+            result = (Book)objIn.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-    /** transport function */
+        return result;
+    }
 
 }
--- src/main/java/server/ServerStub.java
+++ src/main/java/server/ServerStub.java
@@ -2,15 +2,49 @@ package server;
 
 import client.Client;
 
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerStub implements Client {
-    private ServerSocket serverSocket;
-    ServerStub() {
+    ServerStub stub;
 
+    private ServerSocket serverSocket;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private ObjectInputStream objIn;
+    private ObjectOutputStream objOut;
+    private Socket currSocket;
+
+    ServerStub() {
+        try {
+            serverSocket = new ServerSocket(8189);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
+        ServerStub stub = new ServerStub();
+
+        while (true) {
+            stub.listenConn();
+        }
+    }
+
+    private void listenConn() {
+        /* get connection & I/O stream */
+        try {
+            currSocket = serverSocket.accept();
+            in = new BufferedInputStream(currSocket.getInputStream());
+            out = new BufferedOutputStream(currSocket.getOutputStream());
+            objIn = new ObjectInputStream(in);
+            objOut = new ObjectOutputStream(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        in.
 
     }
 
